192f1d9e39c546e734f94545afe819aab3f9c924
gustidev <gustiwebstudio@gmail.com>
2024-11-28 10:45:49 +0800

    28nov2024_win_cashflow1

---
+++ src/app/cashflow/get-data.tsx
@@ -0,0 +1,34 @@
+
+export async function getRL(accountTypeId: number, accountGroup2Id: number) {
+    // const res = await fetch(`${global.baseUrl}/api/neraca?accountTypeId=${accountTypeId}&accountGroup2Id=${accountGroup2Id}`, {
+    //     cache: 'no-store'
+    // })
+    const res = await fetch(`${process.env.APP_URL}/api/neraca?accountTypeId=${accountTypeId}&accountGroup2Id=${accountGroup2Id}`, {
+        cache: 'no-store'
+    })
+    // const res = await fetch(`http:/localhost:3000/api/neraca?accountTypeId=${accountTypeId}&accountGroup2Id=${accountGroup2Id}`, {
+    //     cache: 'no-store'
+    // })
+
+    const data = await res.json()
+    return {
+        accounts: data.accounts,
+        totalBalance: data.totalBalance
+    }
+}
+
+
+export async function getNeracaGroup1(accountTypeId: number, accountGroup1Id: number) {
+    // const res = await fetch(`${global.baseUrl}/api/neraca?accountTypeId=${accountTypeId}&accountGroup2Id=${accountGroup2Id}`, {
+    //     cache: 'no-store'
+    // })
+    const res = await fetch(`${process.env.APP_URL}/api/neraca?accountTypeId=${accountTypeId}&accountGroup2Id=${accountGroup1Id}`, {
+        cache: 'no-store'
+    })
+
+    const data = await res.json()
+    return {
+        accounts: data.accounts,
+        totalBalance: data.totalBalance
+    }
+}
