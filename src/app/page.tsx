3f031281dced69124d16e56e0cec3937f6472afe
gustistudio <gustistudio@192.168.1.5>
2024-11-28 14:13:55 +0800

    mac_add-cashflow-_rem-rl-detail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@
 import Divider from "@/components/Divider";
 import PageLayout from "@/components/PageLayout";
 import global from "@/config.js";
-import { createContext } from "react";
-
-export const TotalRLDetailContext = createContext(999);
 
 export default function Home() {
 
@@ -14,24 +11,23 @@ export default function Home() {
   return (
     <>
 
-      <TotalRLDetailContext.Provider value={999}>
-        <PageLayout header={header} footer={footer}>
+      <PageLayout header={header} footer={footer}>
 
-          <h1 className="text-3xl font-bold">Selamat datang!</h1>
-          <p className="pt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
+        <h1 className="text-3xl font-bold">Selamat datang!</h1>
+        <p className="pt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
 
-          <Divider />
-          <div className="flex flex-1 flex-col gap-4 pt-4 pb-4">
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-              <div className="aspect-video rounded-xl bg-orange-100" />
-              <div className="aspect-video rounded-xl bg-orange-100" />
-              <div className="aspect-video rounded-xl bg-orange-100" />
-            </div>
-            <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        <Divider />
+        <div className="flex flex-1 flex-col gap-4 pt-4 pb-4">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-blue-100" />
+            <div className="aspect-video rounded-xl bg-blue-100" />
+            <div className="aspect-video rounded-xl bg-blue-100" />
           </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
 
-        </PageLayout>
-      </TotalRLDetailContext.Provider>
+      </PageLayout>
+
 
     </>
   );
