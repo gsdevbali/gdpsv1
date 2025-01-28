8980aed30a183020a3eea17859b07850c2c08c2a
gustistudio <gustistudio@gustistudios-iMac.local>
2025-01-28 16:21:19 +0800

    mac_homepage-new-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,22 +22,22 @@ export default function Home() {
         <Divider />
         <div className="flex flex-1 flex-col gap-4 pt-4 pb-5">
           <div className="grid auto-rows-min gap-4 md:grid-cols-5">
-            <MenuBox1 title="JURNAL" desc="Entri Jurnal Umum" menulink='/jurnal'/>
-            <MenuBox1 title="KAS KERK" desc="Entri KAS KERK" menulink='/receive-keb'/>
-            <MenuBox1 title="BANK" desc="Entri Penerimaan BANK" menulink='/receive-other'/>
-            <MenuBox1 title="PENGELUARAN" desc="Entri Pengeluaran" menulink='/expense'/>
-            <MenuBox1 title="AKUN" desc="Daftar Akun" menulink='/coa'/>
+            <MenuBox1 title="JURNAL" desc="Entri Jurnal Umum" menulink='/jurnal' />
+            <MenuBox1 title="KAS KERK" desc="Entri KAS KERK" menulink='/receive-keb' />
+            <MenuBox1 title="BANK" desc="Entri Penerimaan BANK" menulink='/receive-other' />
+            <MenuBox1 title="PENGELUARAN" desc="Entri Pengeluaran" menulink='/expense' />
+            <MenuBox1 title="AKUN" desc="Daftar Akun" menulink='/coa' />
           </div>
 
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <MenuBox2 title="AKTIVITAS" desc="Laporan Aktivitas Bulanan" menulink='/aktivitas'/>
-            <MenuBox2 title="ARUS KAS" desc="Laporan Arus Kas Bulanan" menulink='/cashflow-recap'/>
-            <MenuBox2 title="BUKU BESAR" desc="Laporan Transaksi Buku Besar" menulink='/ledger'/>
+            <MenuBox2 title="AKTIVITAS" desc="Laporan Aktivitas Bulanan" menulink='/aktivitas' />
+            <MenuBox2 title="ARUS KAS" desc="Laporan Arus Kas Bulanan" menulink='/cashflow-recap' />
+            <MenuBox2 title="BUKU BESAR" desc="Laporan Transaksi Buku Besar" menulink='/ledger' />
           </div>
 
           <div className="grid auto-rows-min gap-4 md:grid-cols-2">
-            <MenuBox3 title="PENERIMAAN/PENGELUARAN" desc="Laporan Penerimaan/Pengeluaran Bulanan" menulink='/cashflow'/>
-            <MenuBox3 title="NERACA" desc="Laporan Neraca Bulanan" menulink='/neraca-t'/>
+            <MenuBox3 title="PENERIMAAN/PENGELUARAN" desc="Laporan Penerimaan/Pengeluaran Bulanan" menulink='/cashflow' />
+            <MenuBox3 title="NERACA" desc="Laporan Neraca Bulanan" menulink='/neraca-t' />
           </div>
           <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
         </div>
--- src/components/MenuBox1.tsx
+++ src/components/MenuBox1.tsx
@@ -2,31 +2,31 @@ import { Link } from 'lucide-react'
 import React from 'react'
 
 interface MenuBoxButtonProps {
-    title: string;
-    desc: string;
-    menulink: string;
-  }
+  title: string;
+  desc: string;
+  menulink: string;
+}
 
 function MenuBox1({ title, desc, menulink }: MenuBoxButtonProps) {
 
-    const handleClick = () => {
-        console.log('Button clicked');
-        window.open(menulink, '_self');
-      };
+  const handleClick = () => {
+    console.log('Button clicked');
+    window.open(menulink, '_self');
+  };
 
   return (
-    <div className="aspect-video rounded-xl bg-blue-300 hover:bg-orange-300 cursor-pointer" >
-        <div onClick={handleClick} className="flex flex-col justify-center items-center h-full p-4">
-            <h3 className="text-[1.8em] font-bold text-orange-900">{title}</h3>
-            <p className="text-sm font-bold text-orange-800 text-center">{desc}</p>
-        </div>
+    <div className="aspect-video rounded-xl bg-gray-200 hover:bg-orange-300 cursor-pointer" >
+      <div onClick={handleClick} className="flex flex-col justify-center items-center h-full p-4">
+        <h3 className="text-[1.3em] font-bold text-orange-900 text-center">{title}</h3>
+        <p className="text-sm font-light text-orange-800 text-center">{desc}</p>
+      </div>
     </div>
   )
 }
 
 function handleClick() {
-    window.open('/neraca-t', '_blank');
-    
+  window.open('/neraca-t', '_blank');
+
 }
 
 export default MenuBox1
