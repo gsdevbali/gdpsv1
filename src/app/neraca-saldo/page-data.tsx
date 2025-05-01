98ba571a57d18c0f47c4383e6dc9e3248d034c6d
gsdev-w11 <gsdevbali@gmail.com>
2025-05-01 19:54:53 +0700

    w11_neraca-saldo-new-total

--- src/app/neraca-saldo/page-data.tsx
+++ src/app/neraca-saldo/page-data.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Suspense, useEffect, useState } from "react";
 import React from "react";
@@ -13,34 +13,43 @@ import NeracaDataX from "./neraca-data-x";
 import NeracaDataDebit from "./neraca-data-x-debit";
 import NeracaDataCredit from "./neraca-data-x-credit";
 import NeracaDataX2 from "./neraca-data-x-2";
-
+import NeracaDataTotalDK from "./neraca-data-total-dk";
 
 interface AccountGroup {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
-export default function ShowNSData({ title, accType, accGroup }: { title: string, accType: number; accGroup: number }) {
-    const [accountGroups, setAccountGroups] = useState<AccountGroup[]>([]);
+export default function ShowNSData({
+  title,
+  accType,
+  accGroup,
+}: {
+  title: string;
+  accType: number;
+  accGroup: number;
+}) {
+  const [accountGroups, setAccountGroups] = useState<AccountGroup[]>([]);
 
-    const { start, end, startPrev, endPrev, titleMonthYear, titlePrevMonthYear } = useNeracaSaldoContext();
-    const startPrevX = "2020-01-01";
-    const newTitle = title.toUpperCase();
+  const { start, end, startPrev, endPrev, titleMonthYear, titlePrevMonthYear } =
+    useNeracaSaldoContext();
+  const startPrevX = "2020-01-01";
+  const newTitle = title.toUpperCase();
 
-    useEffect(() => {
-        const fetchGroups = async () => {
-            const groups = await getAccountGroups();
-            setAccountGroups(groups);
-        };
-        fetchGroups();
-    }, [accType])
+  useEffect(() => {
+    const fetchGroups = async () => {
+      const groups = await getAccountGroups();
+      setAccountGroups(groups);
+    };
+    fetchGroups();
+  }, [accType]);
 
-    return (
-        <>
-            <div>
-                <div className="h-4"></div>
+  return (
+    <>
+      <div>
+        <div className="h-4"></div>
 
-                {/* <ShowData title='Penerimaan Persembahan' accType={4} accGroup={8} />
+        {/* <ShowData title='Penerimaan Persembahan' accType={4} accGroup={8} />
                 <div className="h-8"></div>
                 <ShowData title='Penerimaan Lain-lain' accType={4} accGroup={9} />
                 <div className="h-8"></div>
@@ -50,91 +59,123 @@ export default function ShowNSData({ title, accType, accGroup }: { title: string
                 <div className="h-8"></div>
                 <ShowData title='Biaya Bidang & Bapel' accType={5} accGroup={12} /> */}
 
-                {/* <ShowData title='KAS' accType={1} accGroup={1} />
+        {/* <ShowData title='KAS' accType={1} accGroup={1} />
                 <div className="h-8"></div>
 
                 <ShowData title='BANK' accType={1} accGroup={2} />
                 <div className="h-8"></div> */}
 
-                {accountGroups.map((group) => (
-                    <React.Fragment key={group.id}>
-                        <ShowData 
-                            title={group.name.toUpperCase()} 
-                            accType={1} 
-                            accGroup={group.id} 
-                        />
-                        <div className="h-8"></div>
-                    </React.Fragment>
-                ))}
-
-         <div className="h-4"></div>
-                <div className="h-4"></div>
-            </div>
-        </>
-    )
+        {accountGroups.map((group) => (
+          <React.Fragment key={group.id}>
+            <ShowData
+              title={group.name.toUpperCase()}
+              accType={1}
+              accGroup={group.id}
+            />
+            <div className="h-8"></div>
+          </React.Fragment>
+        ))}
+        <Suspense fallback={<Loading section="Tab2" />}>
+            <NeracaDataTotalDK start={start} end={end} />
+        </Suspense>
+        <div className="h-4"></div>
+        <div className="h-4"></div>
+      </div>
+    </>
+  );
 }
 
 //
-function ShowData({ title, accType, accGroup }: { title: string, accType: number; accGroup: number }) {
+function ShowData({
+  title,
+  accType,
+  accGroup,
+}: {
+  title: string;
+  accType: number;
+  accGroup: number;
+}) {
+  const { start, end, startPrev, endPrev, titleMonthYear, titlePrevMonthYear } =
+    useNeracaSaldoContext();
+  const startPrevX = "2020-01-01";
 
-    const { start, end, startPrev, endPrev, titleMonthYear, titlePrevMonthYear } = useNeracaSaldoContext();
-    const startPrevX = "2020-01-01";
+  return (
+    <>
+      <div className="flex flex-row">
+        <div className="w-1/2 pr-2">
+          <div className="flex justify-between">
+            <h2 className="text-blue-600 dark:text-orange-600 font-bold">
+              {title}
+            </h2>
+            {/* <h2 className="text-blue-600 dark:text-orange-600 font-bold">Saldo Awal</h2> */}
+          </div>
+          <Divider />
 
-    return (
-        <>
-            <div className="flex flex-row">
+          {/* kolom AKUN dan Saldo Awal */}
+          <Suspense fallback={<Loading section="Tab1" />}>
+            <NeracaDataX
+              title="Tab1"
+              titleTotal="Tab1"
+              type={accType}
+              group={accGroup}
+              start={startPrevX}
+              end={endPrev}
+            />
+          </Suspense>
+        </div>
 
-                <div className="w-3/4 pr-2">
+        <div className="w-1/2 pr-2">
+          <div>
+            <h2 className="text-center text-blue-600 dark:text-orange-600 font-bold">
+              MUTASI
+            </h2>
+          </div>
+          <Divider />
 
-                    <div className="flex justify-between">
-                        <h2 className="text-blue-600 dark:text-orange-600 font-bold">{title}</h2>
-                        {/* <h2 className="text-blue-600 dark:text-orange-600 font-bold">Saldo Awal</h2> */}
-                    </div>
-                    <Divider />
+          <div className="flex flex-row gap-2">
+            {/* kolom DEBET */}
+            <Suspense fallback={<Loading section="Tab2" />}>
+              <NeracaDataDebit
+                title="Tab2"
+                titleTotal="Tab2"
+                type={accType}
+                group={accGroup}
+                start={start}
+                end={end}
+              />
+            </Suspense>
 
-                    {/* kolom AKUN dan Saldo Awal */}
-                    <Suspense fallback={<Loading section="Tab1" />}>
-                        <NeracaDataX title="Tab1" titleTotal="Tab1" type={accType} group={accGroup} start={startPrevX} end={endPrev} />
-                    </Suspense>
+            {/* kolom KREDIT */}
+            <Suspense fallback={<Loading section="Tab2" />}>
+              <NeracaDataCredit
+                title="Tab2"
+                titleTotal="Tab2"
+                type={accType}
+                group={accGroup}
+                start={start}
+                end={end}
+              />
+            </Suspense>
+          </div>
+        </div>
 
-                </div>
+        {/* <div className="w-1/4"> */}
+        {/* {titleMonthYear} */}
+        {/* <h2 className="text-end text-blue-600 dark:text-orange-600 font-bold">Saldo Akhir</h2> */}
+        {/* <Divider /> */}
 
-                <div className="w-3/5 pr-2">
+        {/* kolom Saldo Akhir */}
+        {/* <Suspense fallback={<Loading section="Tab3" />}> */}
+        {/* <NeracaDataX2 title="Tab3" titleTotal="Tab3" type={accType} group={accGroup} start={start} end={end} /> */}
+        {/* </Suspense> */}
+        {/* </div> */}
+      </div>
 
-                    <div>
-                        <h2 className="text-center text-blue-600 dark:text-orange-600 font-bold">MUTASI</h2>
-                    </div>
-                    <Divider />
-
-                    <div className="flex flex-row gap-2">
-
-                        {/* kolom DEBET */}
-                        <Suspense fallback={<Loading section="Tab2" />}>
-                            <NeracaDataDebit title="Tab2" titleTotal="Tab2" type={accType} group={accGroup} start={start} end={end} />
-                        </Suspense>
-
-                        {/* kolom KREDIT */}
-                        <Suspense fallback={<Loading section="Tab2" />}>
-                            <NeracaDataCredit title="Tab2" titleTotal="Tab2" type={accType} group={accGroup} start={start} end={end} />
-                        </Suspense>
-
-                    </div>
-
-                </div>
-
-
-                {/* <div className="w-1/4"> */}
-                    {/* {titleMonthYear} */}
-                    {/* <h2 className="text-end text-blue-600 dark:text-orange-600 font-bold">Saldo Akhir</h2> */}
-                    {/* <Divider /> */}
-
-                    {/* kolom Saldo Akhir */}
-                    {/* <Suspense fallback={<Loading section="Tab3" />}> */}
-                        {/* <NeracaDataX2 title="Tab3" titleTotal="Tab3" type={accType} group={accGroup} start={start} end={end} /> */}
-                    {/* </Suspense> */}
-                {/* </div> */}
-
-            </div>
-        </>
-    )
+      {/* <div className="w-full">
+                <Suspense fallback={<Loading section="Tab2" />}>
+                    <NeracaDataTotalDebit start={start} end={end} />
+                </Suspense>
+                </div> */}
+    </>
+  );
 }
