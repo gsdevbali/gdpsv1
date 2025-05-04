b5a5df57b4fd71cc36be5fd220f2e9827db6db54
gsdev-w11 <gsdevbali@gmail.com>
2025-05-04 20:55:14 +0700

    w11_tjbj-3

--- src/app/tjbj/neraca-data-x.tsx
+++ src/app/tjbj/neraca-data-x.tsx
@@ -5,17 +5,13 @@ import useAktivitasContext from '@/context/aktivitas-context';
 
 import { DataTable } from "./data-tables";
 import { columns } from "./columns-saldo-nol";
-import SubTotalNol from './total-saldo-nol';
 
-const NeracaDataX = ({ title, titleTotal, type, group2, start, end }: { title: string; titleTotal: string; type: number; group2: number; start: string, end: string }) => {
+const NeracaDataX = ({ key, type, group2, start, end }: { key:string; type: number; group2: number; start: string, end: string }) => {
 
-    // declare here: Var context for AktivitasContext
-    const { setTotalTerima1, setTotalTerima2, setTotalBebanOp, setTotalBeban2, setTotalBeban3 } = useAktivitasContext();
+    const { setTotalTerima1, setTotalTerima2 } = useAktivitasContext();
     // Fetch data using TanStack Query
     const { data: result, isLoading, error, isSuccess } = useQuery({
-        queryKey: ['nsX', type, group2],
-        //queryFn: () => fetch(`/api/neraca?accountTypeId=${type}&accountGroup2Id=${group2}`, { cache: 'no-store' })
-        //queryFn: () => fetch(`/api/neraca-saldo-x?accountTypeId=${type}&accountGroup2Id=${group2}&startDate=${start}&endDate=${end}`, { cache: 'no-store' })
+        queryKey: [{key}, type, group2],
         queryFn: () => fetch(`/api/neraca-saldo-group2?accountGroup2Id=${group2}&startDate=${start}&endDate=${end}`, { cache: 'no-store' })
             .then(response => {
                 if (!response.ok) throw new Error('Network response was not ok');
@@ -27,23 +23,11 @@ const NeracaDataX = ({ title, titleTotal, type, group2, start, end }: { title: s
     if (error) return <div>Error: {error.message}</div>; // Handle error state
     if (!result) return <div>Tidak ada data (null)</div>;
 
-    // const newTotal = Math.abs(totalBalance);
-    // const newTotalBalance = toidr(newTotal)
-
     //Total & data for table
     const { accounts: data, totalBalance, totalDebit, totalCredit } = result;
-    //const newTotal = Math.abs(totalBalance);
-    // const newTotalBalance = toidr(newTotal);
-    // const newTotalDebit = toidr(Math.abs(totalDebit));
-    // const newTotalCredit = toidr(Math.abs(totalCredit));
 
-    //Update Total global States
-    // if (isSuccess) {
-    //     const newTotal = Math.abs(totalBalance);
-    // };
-    //Update Total global States
     if (isSuccess) {
-        //UpdateTotalCF(group2, totalBalance);
+
         const newTotal = Math.abs(totalBalance);
 
         switch (group2) {
@@ -53,17 +37,6 @@ const NeracaDataX = ({ title, titleTotal, type, group2, start, end }: { title: s
                 break;
             case 9:
                 setTotalTerima2(newTotal)
-                break;
-
-            case 10:
-                setTotalBebanOp(newTotal)
-
-            case 11:
-                setTotalBeban2(newTotal)
-                break;
-
-            case 12:
-                setTotalBeban3(newTotal)
                 break;
 
             default:
@@ -76,9 +49,6 @@ const NeracaDataX = ({ title, titleTotal, type, group2, start, end }: { title: s
         <>
             <div className="w-full">
                 <DataTable columns={columns} data={data} />
-                
-                {/* <SubTotalNol /> */}
-                {/* <SubTotalAktivitasBefore value={newTotalBalance} title={titleTotal} />                 */}
             </div>
         </>
     )
--- src/app/tjbj/page-data.tsx
+++ src/app/tjbj/page-data.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Suspense } from "react";
 
@@ -12,79 +12,106 @@ import NeracaDataX from "./neraca-data-x";
 import SubTotalTerima from "./total-terima";
 import SubTotalRekap from "./total-rekap";
 
-export default function ShowData({ title, start, end }: { title: string; start: string; end: string }) {
-
-    return (
-        <>
-            <div>
-                <div className="h-4"></div>
-                <ShowDataTotal />
-                <div className="h-4"></div>
-                <ShowNSData title='Penerimaan Persembahan' accType={4} accGroup={8} />
-                <div className="h-8"></div>
-                <ShowNSData title='Penerimaan Lain-lain' accType={4} accGroup={9}  />
-                <div className="h-8"></div>
-                <ShowDataTotal />
-                <div className="h-4"></div>
-
-            </div>
-        </>
-    )
+export default function ShowData({
+  title,
+  start,
+  end,
+}: {
+  title: string;
+  start: string;
+  end: string;
+}) {
+  return (
+    <>
+      <div>
+        <div className="h-4"></div>
+        <ShowDataTotal />
+        <div className="h-4"></div>
+        <ShowNSData title="Penerimaan Persembahan" accType={4} accGroup={8} />
+        <div className="h-8"></div>
+        <ShowNSData title="Penerimaan Lain-lain" accType={4} accGroup={9} />
+        <div className="h-8"></div>
+        <ShowDataTotal />
+        <div className="h-4"></div>
+      </div>
+    </>
+  );
 }
 
 //
-function ShowNSData({ title, accType, accGroup }: { title: string, accType: number; accGroup: number }) {
+function ShowNSData({
+  title,
+  accType,
+  accGroup,
+}: {
+  title: string;
+  accType: number;
+  accGroup: number;
+}) {
+  const { totalTerima1, totalTerima2 } = useAktivitasContext();
+  const { start, end } = useNeracaSaldoContext();
 
-    const { totalTerima1, totalTerima2 } = useAktivitasContext();    
-    const { start, end } = useNeracaSaldoContext();
+  const subValue =
+    // accGroup === 8 ? totalTerima1 : accGroup === 9 ? totalTerima2 : 0;
+    accGroup === 8 ? totalTerima1 : totalTerima2;
 
-    const subValue = accGroup === 8 ? totalTerima1 : accGroup === 9 ? totalTerima2 : 0;
+  return (
+    <>
+      <div className="flex flex-row">
 
-    return (
-        <>
-            <div className="flex flex-row">
+        <div className="w-full pr-2">
+          <div className="flex justify-between">
+            <h2 className="text-blue-600 dark:text-orange-600 font-bold">
+              {title}
+            </h2>
+            {/* <h2 className="text-blue-600 dark:text-orange-600 font-bold">Saldo Awal</h2> */}
+          </div>
+          <Divider />
 
-                {/* KELOMPOK AKUN & SALDO AWAL */}
-                <div className="w-full pr-2">
-                    <div className="flex justify-between">
-                        <h2 className="text-blue-600 dark:text-orange-600 font-bold">{title}</h2>
-                        {/* <h2 className="text-blue-600 dark:text-orange-600 font-bold">Saldo Awal</h2> */}
-                    </div>
-                    <Divider />
+          {/* Data */}
+          <Suspense fallback={<Loading section="Tab1" />}>
+            <NeracaDataX
+              key={title}
+              type={accType}
+              group2={accGroup}
+              start={start}
+              end={end}
+            />
+            <SubTotalTerima value={toidr(subValue)} title={title} />
+          </Suspense>
+        </div>
 
-                    {/* kolom AKUN dan Saldo Awal */}
-                    <Suspense fallback={<Loading section="Tab1" />}>
-                        <NeracaDataX title="Tab1" titleTotal="Tab1" type={accType} group2={accGroup} start={start} end={end} />
-                        <SubTotalTerima value={toidr(subValue)} title={title} />
-                    </Suspense>
-                </div>
-
-                {/* SALDO AKHIR */}
-
-
-            </div>
-        </>
-    )
+        {/* SALDO AKHIR */}
+      </div>
+    </>
+  );
 }
-
 
 //
 function ShowDataTotal() {
-    
-    const { totalTerima1, totalTerima2 } = useAktivitasContext();
+  const { totalTerima1, totalTerima2 } = useAktivitasContext();
 
-    const totalPenerimaan = totalTerima1 + totalTerima2;
-    const totalKlasis = 0.04 * totalPenerimaan;
-    const totalSinwil = 0.1 * totalPenerimaan;
+  const totalPenerimaan = totalTerima1 + totalTerima2;
+  const totalKlasis = 0.04 * totalPenerimaan;
+  const totalSinwil = 0.1 * totalPenerimaan;
 
-    return (
-        <>
-        <div className="bg-slate-300 dark:bg-gray-800 p-4 rounded-md shadow-md">
-        <SubTotalRekap value={toidr(totalPenerimaan)} title="Total Penerimaan" />
-        <SubTotalRekap value={toidr(totalKlasis)} title="Total TJBJ Klasis (4%)" />
-        <SubTotalRekap value={toidr(totalSinwil)} title="Total TJBJ Sinwil (10%)" />
-        <Divider/>
-        </div>
-        </>    
-    );
+  return (
+    <>
+      <div className="bg-slate-300 dark:bg-gray-800 p-4 rounded-md shadow-md">
+        <SubTotalRekap
+          value={toidr(totalPenerimaan)}
+          title="Total Penerimaan"
+        />
+        <SubTotalRekap
+          value={toidr(totalKlasis)}
+          title="Total TJBJ Klasis (4%)"
+        />
+        <SubTotalRekap
+          value={toidr(totalSinwil)}
+          title="Total TJBJ Sinwil (10%)"
+        />
+        <Divider />
+      </div>
+    </>
+  );
 }
